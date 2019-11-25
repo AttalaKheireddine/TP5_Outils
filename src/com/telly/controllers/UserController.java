744210651c2ecef27533d5c358e8f104072e3b6e
ATTALA Kheireddine <gk_attala@esi.dz>
2019-11-25 09:12:53 +0100

    added disconnection

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -9,10 +9,16 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class UserController {
     @RequestMapping("/login")
     public String showLogin() {
         return "login";
+    }
+
+    @RequestMapping("/loggedout")
+    public String showLogout() {
+        return "loggedout";
     }
 }
 

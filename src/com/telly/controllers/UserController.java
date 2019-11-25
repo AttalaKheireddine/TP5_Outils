df06a6a45bb0d9a140cdd0cebe26dba471b78427
unknown <abdellatif201111@gmail.com>
2019-11-25 10:20:17 +0100

    comm

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -1,10 +1,11 @@
+package com.telly.controllers;
 
 import java.security.Principal;
-package com.telly.controllers;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframeimport rg.springframework.ui.Model;
+import org.springframeimport.rg.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,8 @@ import com.telly.dao.Reserve;
 import com.telly.service.ReserveService;
 
 @Controler
-
 class UserController {
+	
     @RequestMapping("/createaccount")
     public String createAccount(Model model, Principal principal) {
 
@@ -50,39 +51,4 @@ class UserController {
     public String showLogout() {
         return "loggedout";
     }
-
-    Reserv
-
-    public String createReserveBook(@Validated(FormValidationGroup.class) Reserve reserve, BindingResult result, Principal principal) {
-		
-		if (result.hasErrors()) {
-			return "reservebus";
-		}
-		
-		String username = principal.getName();
-		reserve.getU
-    	
-
-    
-    	
-            
-
-        
-             }
-
-    ublic String
-
-    getReserveBook(@Validated
-        
-
-        String username = principal.getN
-
-        List<Reserve> reserves = reserveService.getReserves(username);
-		model.addAttribute("reserves", reserves);
-		Sys
-
-    
-		return "home";
-
-	}
 }
